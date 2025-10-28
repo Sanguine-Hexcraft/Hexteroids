22a91a72e0b457556e60f0cff33c28f84a549a5c
sanguine-hexcraft <hexcraft-apothecary@gmail.com>
2025-10-27 21:31:14 -0600

    added pygame display dimensions, fill with black, refresh (flip()), and a quit check

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+# this allows us to use code from
+# the open-source pygame library
+# through this file
+import pygame
+from constants import *
+
+
 def main():
-    print("Hello from hexteroids!")
+    # Initialize pygame
+    pygame.init()
+    
+    # Use pygame's display.set.mode() to get a new GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # Use an infinite while loop for the game loop.
+    while True:
+        # This will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # At each iteration, it should
+        # Use the screen's fill method to fill the screen with a solid "black" color
+        screen.fill((0,0,0,))
+        # Use pygame's display.flip() method to refresh the screen. Be sure to call this last!
+        pygame.display.flip()
+    
 if __name__ == "__main__":
     main()
